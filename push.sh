7ee6b92c714579a980815233f2b3fd0aae10b4e7
TengMCing <llreczx@gmail.com>
2021-05-20 01:12:13 +1000

    Updated: 2021-05-20 01:12:13

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 open down.html
-sleep 10
+sleep 5
 mv ~/Downloads/spotoroo-publicleaderboard.zip ./data/spotoroo-publicleaderboard.zip
 git log --follow --reverse -p data/spotoroo-publicleaderboard.csv > data/log.txt
 python3 kaggle_lead.py
